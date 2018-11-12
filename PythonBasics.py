bff20a7c11ce4d7e33042b225422773fba9ff746
iuztemur <uztemurismail@gmail.com>
2018-11-13 02:23:56 +0300

    file name has changed && dictionary file added && development continuous.

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,36 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+# Python Basics Begin
+# Tis script includes(Variables, String, Numbers, Built In, User Defined, Default, Flexible Function, Lambda Function
+#  and Quiz1(Quiz1.py))
+# Author Ismail Uztemur
 
 # %% => bunun anlami bir section oldugunu gosterir ve bu sectionu run etmek icin Shift + Enter
 
-#variables (degisken)
-#functions
-#objects
-var1 = 10 #integer
+#%% Variables -> First Section
+
+# variables(degisken)
+# Functions
+# Objects
+
+var1 = 10  # integer
 var2 = 15
-var3 = 10.0 #double or float
-#5var = 10  #hata verir
-gun = "pazartesi" #string
+var3 = 10.0  # double or float (ondalikli rakamlar.)
+# 5var = 10  #hata verir
 
-#variable lar standart conventon of python'a gore buyuk harfle baslamasi uygun degil.
-#Comment ve tanimlamalarda turkce karakterler kullanilmaz.
+gun = "pazartesi"
+# string
 
-#%%  => Ikinci Section 
-#string 
-s = "bugun gunlerden Pazartesi"
+# variable lar standard convention of python'a gore buyuk harfle baslamasi uygun degil.
+# Comment ve tanimlamalarda turkce karakterler kullanilmaz.
 
-variable_type = type(s) #burada tanimlanan variable tipinin ne oldugunu ogreniriz. (str = string)
+
+#%% String => Second Section
+
+s = "bugun gunlerden pazartesi"
+
+variable_type = type(s)  # burada tanimlanan variable tipinin ne oldugunu ogreniriz. (str = string)
 
 print(s)
 
 var1 = "ankara"
-var2 = "istanbul"
-var3 = var1 + var2
+var2 = "ist"
+var3 = var1+var2
 
-print(var3)
+var4 = "100"
+var5 = "200"
+var6 = var4+var5
 
-uzunluk = len(var3)
+uzunluk = len(var6)
+
+#%% Numbers -> Third Section
+
+# int
+integer_deneme = -50
+# double = float = ondalikli sayi
+float_deneme = -30.7
+
+#%% Built In Functions -> Fourth Section
+
---
+++ WorkingNotes.py
@@ -0,0 +1,11 @@
+# This is the python programming working notes.
+
+#%%
+
+var = 10
+
+isim = "Ismail"
+
+var2 = 10.4
+
+# Degisken isimlerinde boşluk olmaz.
